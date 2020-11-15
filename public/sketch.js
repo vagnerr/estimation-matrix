47a3587146b9cc60cb0c2c433fafdc8276b2f449
Peter Wise <peter@bloodaxe.com>
2020-11-15 16:14:48 +0000

    fix connect to go back to origin. improve show/hide functionality switch to mouseClick

--- public/sketch.js
+++ public/sketch.js
@@ -1,25 +1,53 @@
 //const { text } = require("express");
 
 var socket;
+var visible     = false;
+var lastdata    = null;    // Last dataset of all users choices
+var lastchoice  = null;  // last choice by current user
+const canvasX = 500;
+const canvasY = 500;
 
 function setup() {
   // put setup code here
-  createCanvas(500,500);
+  createCanvas(canvasX,canvasY);
   background(51);
   resetCanvas();
-  socket = io.connect('http://172.16.1.26:3000');
+
+  socket = io.connect();
   socket.on('mouse', newDrawing);
   socket.on('show', revealAll);
-  socket.on('hide', resetCanvas);
+  socket.on('hide', hideCanvas);
   socket.on('reset', resetCanvas);
+}
+
+
+
+
+function hideCanvas() {
+  console.log('canvas Hidden');
+  visible=false;
+
+  redrawCanvas();
+  // Put current users original choice back
+  if (lastchoice) {
+    fill(0, 255, 0);
+    ellipse(lastchoice.x, lastchoice.y, 6, 6);
+  }
+
 }
 
 function resetCanvas() {
   console.log('canvas reset');
+  visible=false;
+
+  redrawCanvas();
+}
+
+function redrawCanvas() {
   clear();
   background(51);
   stroke(255);
-  line(167,0,167,500);
+  line(167,0,167,500);  // TODO convert all this into fractions of canvasX/Y
   line(333,0,333,500);
   line(0,167,500,167);
   line(0,333,500,333);
@@ -38,35 +66,57 @@ function resetCanvas() {
 
 function revealAll(data){
   console.log('Showing all marks');
-  resetCanvas() // deals with drawover
+  visible = true;
+  lastdata = data;   // keep local copy for refreshes
+  redrawCanvas() // deals with drawover
   console.log(data['last']);
   //map(data['last']).forEach((value, key) => {
-  for (var sid in data['last']){
-    if (
-      data['last'][sid].x &&
+  drawEstimates(data);
+}
+
+function drawEstimates(data) {
+  for (var sid in data['last']) {
+    if (data['last'][sid].x &&
       data['last'][sid].y &&
-      data['name'][sid]
-    ){
+      data['name'][sid]) {
       noStroke();
-      fill(0,255,0);
-      ellipse(data['last'][sid].x,data['last'][sid].y,6,6);
-      fill(0,180,50);
+      fill(0, 255, 0);
+      ellipse(data['last'][sid].x, data['last'][sid].y, 6, 6);
+      fill(0, 180, 50);
       textSize(8);
       text(
-            data['name'][sid],
-            data['last'][sid].x+5,
-            data['last'][sid].y
-          );
+        data['name'][sid],
+        data['last'][sid].x + 5,
+        data['last'][sid].y
+      );
     }
   }
 }
+
 function newDrawing(data){
   noStroke();
   fill(255,0,100);
   ellipse(data.x, data.y, 36,36);
 }
 
-function mouseDragged() {
+function mouseClicked() {
+  // Canvas check
+  // Ignore the click if its OUTSIDE the canvas area
+  // otherwise it will pickup the click events on the
+  // control buttons
+  if (   mouseX < 0
+      || mouseY < 0
+      || mouseX > canvasX
+      || mouseY > canvasY){
+        return
+      }
+
+
+  redrawCanvas();
+  if( visible && lastdata ){
+    console.log('drawing everything else');
+    drawEstimates(lastdata);
+  }
   console.log(mouseX + ', ' + mouseY);
   noStroke();
   fill(255,100,100);
@@ -76,6 +126,7 @@ function mouseDragged() {
     x: mouseX,
     y: mouseY
   }
+  lastchoice = data;
   socket.emit('mouse', data);
 }
 function draw() {
