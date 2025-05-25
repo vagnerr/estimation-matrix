02b7a1dfc9d50e965644845d0b70a24f1929439d
Peter Wise <peter@bloodaxe.com>
2025-05-25 23:34:18 +0100

    Break state control and messaging into separate rooms

--- server.js
+++ server.js
@@ -58,24 +58,60 @@ function newConnection(socket) {
   function processCommand(data) {
     console.log('command: ' + data.command);
 
+    const target_room = state['room'][socket.id] || null;
+    const filtered_state = getFilteredState(target_room);
     switch(data.command) {
       case 'show':
-        io.sockets.emit('show',state);
+        io.to(target_room).emit('show',filtered_state);
         break;
       case 'hide':
-        io.sockets.emit('hide');
+        io.to(target_room).emit('hide');
         break;
       case 'reset':
-        io.sockets.emit('reset');
-        state['last'] = {};   // Reset all the points, but keep names
+        io.to(target_room).emit('reset');
+        resetRoom(target_room);
         broadcastUserState()
         break;
       default:
         console.log('Unknown command: ' + data.command);
     }
-    console.log('STATE: ');
+    //console.log('STATE: ');
     console.log(state);
   }
+
+  function resetRoom(target_room) {
+    const users_in_room = Object.keys(state['room']).filter(id => state['room'][id] === target_room);
+
+    for (let id of users_in_room) {
+      state['last'][id] = null;  // Reset the last point for each user in the room
+    }
+
+  }
+
+  function getFilteredState(target_room) {
+    // Filter the state to only include users in the target room
+    const users_in_room = Object.keys(state['room']).filter(id => state['room'][id] === target_room);
+
+
+    let filtered_state = {
+      name: {},
+      last: {},
+      disconnect: {},
+      room: {}
+    };
+
+    for (let id of users_in_room) {
+      filtered_state['name'][id] = state['name'][id];
+      filtered_state['last'][id] = state['last'][id];
+      filtered_state['disconnect'][id] = state['disconnect'][id];
+      filtered_state['room'][id] = state['room'][id];
+    }
+
+    // console.log('raw state: ', state);
+    // console.log('Filtered State: ', filtered_state);
+    return filtered_state;
+  }
+
 
   function receiveUserName(data) {
     console.log('Name: ' + data.name);
@@ -92,7 +128,13 @@ function newConnection(socket) {
    */
   function broadcastUserState() {  // TODO breakup by room
     let user_state = {}
+    let target_room = state['room'][socket.id] || null;
+    console.log(`broadcastUserState: target_room=${target_room}`);
     for (let id in state['name']){
+      // If the user is in a room, only send them the users in that room
+      if (!target_room || state['room'][id] !== target_room) {
+        continue;  // skip users not in the same room
+      }
       user_state[id]={
         name: state['name'][id],
         active: state['disconnect'][id]?false:true,
@@ -100,7 +142,8 @@ function newConnection(socket) {
         room: state['room']?state['room'][id]:null
       }
     }
-    io.sockets.emit('users',user_state);
+    //console.log('emitting_state: ' + JSON.stringify(user_state)); // TODO bug: emitting all rooms on mouse vote.
+    io.to(target_room).emit('users',user_state);
     return user_state
   }
 
@@ -110,12 +153,14 @@ function newConnection(socket) {
     delete state['name'][id];
     delete state['last'][id];
     delete state['disconnect'][id];
+    delete state['room'][id];
   }
   function setRoom(room) {
     console.log(`Client ${socket.id} joining room: ` + JSON.stringify(room));
     state['room'][socket.id] = room.room;
+    socket.join(room.room);
     let user_state = broadcastUserState();
-    console.log(user_state);
+    //console.log(user_state);
     // socket.join(room);
     // state[socket.id].room = room;
   }
